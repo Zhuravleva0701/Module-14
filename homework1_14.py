db85103fc1db4a33d55848d6ef09b8843b1d13f2
Mariia Zhuravleva <167981362+Zhuravleva0701@users.noreply.github.com>
2024-09-04 08:17:03 +0300

    Add files via upload

--- homework1_14.py
+++ homework1_14.py
@@ -1,5 +1,7 @@
 import sqlite3
 
+# Код из предыдущего задания
+
 connection = sqlite3.connect('not_telegram.db')
 cursor = connection.cursor()
 
@@ -26,10 +28,25 @@ cursor.execute('CREATE INDEX IF NOT EXISTS idx_email ON Users (email)')
 # for i in range(1, 11, 3):
 #     cursor.execute('DELETE FROM Users WHERE id = ?', (i, ))
 
-cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != ?', (60, ))
-users = cursor.fetchall()
-for user in users:
-    print(user)
+# cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != ?', (60, ))
+# users = cursor.fetchall()
+# for user in users:
+#     print(user)
+
+# Удаление пользователя с id=6
+# cursor.execute('DELETE FROM Users WHERE id = ?', (6, ))
+
+# Подсчёт кол-ва всех пользователей
+cursor.execute('SELECT COUNT(*) FROM Users')
+total_users = cursor.fetchone()[0]
+# print(total_users)
+
+# Подсчёт суммы всех балансов
+cursor.execute('SELECT SUM(balance) FROM Users')
+all_balances = cursor.fetchone()[0]
+# print(all_balances)
+print(all_balances / total_users)
+
 
 connection.commit()
 connection.close()
